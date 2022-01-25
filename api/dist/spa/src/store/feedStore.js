c92ee11b0def35a9ef1021cddfdd6ba3af7c5ea9
David Ivkovic <gorajkora@gmail.com>
2022-01-25 20:30:30 +0100

    Izmenjen store za paginaciju feeda

--- api/dist/spa/src/store/feedStore.js
+++ api/dist/spa/src/store/feedStore.js
@@ -1,147 +1,42 @@
-const posts = [
-  {
-    id: 1,
-    imageURL:
-      'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    caption: 'Where flowers bloom, so does hope',
-    timestamp: '2022-01-16T17:27:25.416937200',
-    comments: [
-      {
-        user: {
-          username: 'jennie',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
+import { isAuthenticated } from './userStore.js'
+import { reactive, ref } from '/modules/vue.js'
+import api from '/src/api/api.js'
+const posts = ref([])
+const suggestedUsers = reactive([])
+const exhausted = ref(false)
+const isLoading = ref(false)
 
-        timestamp: '2022-01-17T17:27:25.416937200',
-        content: 'So pretty!',
-      },
+const fetchPosts = async (initial, isExplore = false) => {
+  if (initial && posts.value.length) return
+  if (exhausted.value || isLoading.value) return
+  isLoading.value = true
 
-      {
-        user: {
-          username: 'lera.ns',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
+  if (isExplore) {
+    var [data] = await api.posts.explore(
+      initial ? '' : posts.value.at(-1)?.timestamp
+    )
+  } else {
+    var [data] = await api.posts.feed(
+      initial ? '' : posts.value.at(-1)?.timestamp
+    )
+  }
 
-        timestamp: '2022-01-17T10:27:25.416937200',
-        content: 'Cutee!',
-      },
-    ],
+  if (data) {
+    posts.value.push(...data)
+    data.length == 0 && (exhausted.value = true)
+  }
 
-    commentCount: 2,
-    poster: {
-      username: 'miyayeah',
-      imageURL:
-        'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    },
-  },
+  isLoading.value = false
+}
 
-  {
-    id: 2,
-    imageURL:
-      'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    caption: 'Where flowers bloom, so does hope',
-    timestamp: '2022-01-16T17:27:25.416937200',
-    comments: [
-      {
-        user: {
-          username: 'jennie',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
+const newPost = post => posts.value.unshift(post)
 
-        timestamp: '2022-01-17T17:27:25.416937200',
-        content: 'So pretty!',
-      },
+const removePost = post =>
+  (posts.value = posts.value.filter(p => p.id != post.id))
 
-      {
-        user: {
-          username: 'lera.ns',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
+if (isAuthenticated.value) {
+  const [data] = await api.users.suggestUsers()
+  data && suggestedUsers.push(...data)
+}
 
-        timestamp: '2022-01-17T10:27:25.416937200',
-        content: 'Cutee!',
-      },
-    ],
-
-    commentCount: 2,
-    poster: {
-      username: 'miyayeah',
-      imageURL:
-        'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    },
-  },
-
-  {
-    id: 3,
-    imageURL:
-      'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    caption: 'Where flowers bloom, so does hope',
-    timestamp: '2022-01-16T17:27:25.416937200',
-    comments: [
-      {
-        user: {
-          username: 'jennie',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
-
-        timestamp: '2022-01-17T17:27:25.416937200',
-        content: 'So pretty!',
-      },
-
-      {
-        user: {
-          username: 'lera.ns',
-          imageURL:
-            'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-        },
-
-        timestamp: '2022-01-17T10:27:25.416937200',
-        content: 'Cutee!',
-      },
-    ],
-
-    commentCount: 2,
-    poster: {
-      username: 'miyayeah',
-      imageURL:
-        'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-    },
-  },
-]
-
-const users = [
-  {
-    imageURL:
-      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGdpcmwlMjBwcm9maWxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    username: 'boca_00',
-    mutualsCount: 5,
-  },
-
-  {
-    imageURL:
-      'https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGdpcmwlMjBwcm9maWxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    username: 'jelenaa',
-    mutualsCount: 10,
-  },
-
-  {
-    imageURL:
-      'https://images.unsplash.com/photo-1634316887741-93ff860c6854?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTIwfHxnaXJsJTIwcHJvZmlsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60',
-    username: 'filipovics',
-    mutualsCount: 25,
-  },
-
-  {
-    imageURL:
-      'https://images.unsplash.com/photo-1524117074681-31bd4de22ad3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGdpcmwlMjBwcm9maWxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    username: 'snezanaart',
-    mutualsCount: 14,
-  },
-]
-
-export { posts, users }
+export { posts, fetchPosts, newPost, removePost, suggestedUsers }
--- api/dist/spa/src/store/userStore.js
+++ api/dist/spa/src/store/userStore.js
@@ -1,27 +1,17 @@
-import { computed, reactive } from '/modules/vue.js'
-
+import { computed, reactive, ref } from '/modules/vue.js'
 const authenticatedUser = JSON.parse(localStorage.getItem('authenticated-user'))
-let user = reactive({
-  // imageURL: 'https://i.pinimg.com/originals/04/e1/7a/04e17aadb6a029518f4b2a45f7053c65.png',
-  // username: 'dracaa.m',
-  // isAdmin: false
-  ...authenticatedUser,
-})
+let user = reactive({ ...authenticatedUser })
 
 const setUser = newUser => {
-  console.log(user)
-
   Object.assign(user, newUser)
-  console.log(user)
   localStorage.setItem('authenticated-user', JSON.stringify(newUser))
 }
 
 const removeUser = () => {
   user = reactive({})
-  console.log(user)
   localStorage.removeItem('authenticated-user')
 }
 
 const isAuthenticated = computed(() => Object.keys(user).length !== 0)
-
-export { user, setUser, removeUser, isAuthenticated }
+const hasNotification = ref(false)
+export { user, setUser, removeUser, isAuthenticated, hasNotification }
