e5e5810ad5c5758201ade1be15c62ab295aa7332
milicad00 <draca.milica10@gmail.com>
2022-01-27 09:40:34 +0100

    Dodate ikonice

---
+++ api/dist/spa/src/icons/EmptyCheckIcon.js
@@ -0,0 +1,24 @@
+export default {
+  template: `  
+  	<svg
+  		aria-label="Toggle selection"
+  		class="_8-yf5"
+  		color="#262626"
+  		fill="#262626"
+  		height="24"
+  		role="img"
+  		viewBox="0 0 24 24"
+  		width="24"
+  	>
+  		<circle
+  			cx="12.008"
+  			cy="12"
+  			fill="none"
+  			r="11.25"
+  			stroke="currentColor"
+  			stroke-linejoin="round"
+  			stroke-width="1.5"
+  		/>
+  	</svg>
+  `,
+}
